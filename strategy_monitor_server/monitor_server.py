7e30ca85ad769843fa2c897fedc4e5494af8627a
双木成林 <459057268@@qq.com>
2021-03-26 18:49:08 +0800

    加密机制

--- strategy_monitor_server/monitor_server.py
+++ strategy_monitor_server/monitor_server.py
@@ -2,29 +2,41 @@ import zmq
 import sys
 import _thread
 import json
+from Crypto.Cipher import AES
 from strategy_monitor_server import strategy_info
 from strategy_monitor_server import log_msg
 from strategy_monitor_server import order_msg
+from strategy_monitor_server import config
 
 deal_funs = {}
 
-def run_server(port):
-    _thread.start_new_thread(run_thread,(port,))
+def run_server():
+    _thread.start_new_thread(run_thread)
 
-def run_thread(port):
+def run_thread():
     #注册子处理函数
     reg_deal_fun
     #启动服务
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind(f"tcp://*:{port}")
+    socket.bind(f"tcp://*:{config.PORT}")
     while True:
         try:
-            json_str = socket.recv()
+            json_bytes = socket.recv()
+            json_str = decrypt(json_bytes).decode()
             result = deal_msg(json_str)
             socket.send(result)
         except Exception as e:
             print('异常:',e)
+
+#解密
+def decrypt(content):
+    iv = "hqiFLiPTJkk9xDSA".encode()
+    key = config.PASSWORD.encode()
+    while len(key) % 16 != 0:
+        key += b'\x00'
+    aes = AES.new(config.PASSWORD,AES.MODE_CBC,iv)
+    return aes.decrypt(content)
 
 #注册子处理函数
 def reg_deal_fun():
